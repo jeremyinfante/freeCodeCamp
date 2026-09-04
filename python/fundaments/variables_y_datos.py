b8eeba474f46052f0d4a4f3b726060b4afcb152e
jere <jeremyjonasinfantemonegro@gmail.com>
2026-09-04 18:59:26 -0400

    first commit

---
+++ python/fundaments/variables_y_datos.py
@@ -0,0 +1,11 @@
+name = 'John Doe' # str
+age = 25 # int
+male = True # bool 
+money = 82.04 # float
+
+# imprimir por pantalla
+print(name, age, male, money)
+
+# verificar el tipo de dato usando type()
+print(type(name))
+print(isinstance(name, int))

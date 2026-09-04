3bed4b1d0b03d76edf6ebfb9ed7a730a2ff6fd93
jere <jeremyjonasinfantemonegro@gmail.com>
2026-09-04 19:04:51 -0400

    isinstance

--- python/fundamentos/variables_y_datos.py
+++ python/fundamentos/variables_y_datos.py
@@ -8,4 +8,9 @@ print(name, age, male, money)
 
 # verificar el tipo de dato usando type()
 print(type(name))
-print(isinstance(name, int))
+
+# verificar tipo de dato usando isinstance()
+# compara entre tipos de datos
+# devuelve un valor booleano
+print(isinstance(name, int)) # False
+print(isinstance(name, str)) # True
